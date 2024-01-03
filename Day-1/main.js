5f0f7751de67efaa251c107a75c2bf834325d295
ducanhaovai <ducanhaovai@users.noreply.github.com>
2024-01-03 22:38:30 +0700

    Js, Html, css
    học 1 số thứ cơ bản của js và làm 1 ít phần cắt giao diện

---
+++ Day-1/main.js
@@ -0,0 +1,15 @@
+var name = "mai";
+var age="20";
+
+console.log("tên là: ", name);
+
+//toan tu
+var x,y;
+x= 10, x =2 ;
+console.log("x += y", x+=y);
+console.log("x = ", x);
+
+// toan tu typeof => xac dinh kieu du lieu cua bien tai thoi diem do.
+console.log("typeof x= ", typeof x);
+console.log("typeof name", typeof name);
+
---
+++ Day-1/main2.js
@@ -0,0 +1,7 @@
+var age = 16;
+console.log("age = ", age);
+console.log("age >= ", age >= 20);
+console.log("age < ", age < 30);
+console.log("!(age <30) ", !age < 30);
+
+console.log("age >= 20 && age < 30")
